05e7d7ee5e73c0fdfdbaa37bc64814b1e9c800e8
MorskoySom <Somovvitvit@gmail.com>
2023-10-13 18:31:41 +0300

    befStyled

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { Btn } from "Button/Button";
+import { ImageGallery } from "ImageGallery/ImageGallery";
+import { Loader } from "Loader/Loader";
+import { Searchbar } from "Searchbar/Searchbar";
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    query: "",
+    page: 1,
+    galleryItems: [],
+    loading: false,
+    error: false,
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    // зберігаємо query - параметр пошуку
+    // скидаємо page до 1
+    // очистити масив galleryItems
+  }
+
+  handleLoadMore = () => {
+    this.setState(prevState => prevState.page + 1);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      // HTTP-запит
+    }
+  }
+
+  render() {
+    return <div>
+      <Searchbar />
+      {this.state.galleryItems.length > 0 && <ImageGallery />}
+      <ImageGallery />
+      <Btn onClick={this.handleLoadMore} />
+      {this.state.loading && <Loader />}
+    </div>;
+
+  }
+}
---
+++ src/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,4 @@
+export const ImageGallery = () => {
+    return (<ul class="gallery">Gallery
+    </ul>)
+}
